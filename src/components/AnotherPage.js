22cddf7e3539cab8a228faa09b0a7e4471ce8d6e
Mariam Mirzoyan <marmirzoyan96@gmail.com>
2023-09-29 22:18:11 +0400

    changed color of page heading

--- src/components/AnotherPage.js
+++ src/components/AnotherPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AnotherPage() {
   return (
     <div>
-      <h1>This is AnotherPage Section</h1>
+      <h1 className='page-heading'>This is AnotherPage Section</h1>
       <p>Another page is a placeholder</p>
     </div>
   )
